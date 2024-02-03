b97cc2b171a662f4425c17cc782074d1609b84f0
Justin Nicholas Dubin <99613905+Jdubin1417@users.noreply.github.com>
2024-02-02 20:27:03 -0500

    Update Program.cs

--- HighLow/Program.cs
+++ HighLow/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 class HighLowGame
 {
     static void Main()
@@ -20,11 +20,14 @@ class HighLowGame
             Random random = new Random(); // Initializes random number generator
             int randomNumber = random.Next(1, 11); // Generates random number between 1 and 10
             int attempts = 0; // Tracks number of attempts the user has remaining and resets it for each playthrough
-            bool isGuessedCorrectly = false;
+            bool isGuessedCorrectly = false; // To track if the user guessed correctly
+            int lastGuess = -1; // To track the last guess for checking repeated guesses
+            string expectedDirection = ""; // No direction expected initially
+            int AmountofGuesses = 5; // To track the amount of guesses allowed
 
-            while (!isGuessedCorrectly && attempts < 5)
+            while (!isGuessedCorrectly && attempts < AmountofGuesses)
             {
-                Console.WriteLine($"Guess a number between 1 and 10. Attempts left: {5 - attempts}");
+                Console.WriteLine($"Guess a number between 1 and 10. {AmountofGuesses - attempts} Attempts Left!");
                 string userInput = Console.ReadLine();  // Reads user input as a string
                 int guess; // Declares guess variable
 
@@ -42,14 +45,32 @@ class HighLowGame
                     continue;
                 }
 
+                // Check if the user repeated the last guess
+                if (guess == lastGuess)
+                {
+                    Console.WriteLine("You're a goofball and I don't want to play anymore.");
+                    playAgain = false; // Exit the game immediately
+                    break; // Exit the guessing loop
+                }
+
+                // Check if the user is going in the wrong direction
+                if ((expectedDirection == "higher" && guess <= lastGuess) || (expectedDirection == "lower" && guess >= lastGuess))
+                {
+                    Console.WriteLine("You're a goofball and I don't want to play anymore.");
+                    playAgain = false; // Exit the game immediately
+                    break; // Exit the guessing loop
+                }
+
                 // Checks if user input is equal to, higher than, or lower than the random number
                 if (guess < randomNumber)
                 {
                     Console.WriteLine("Higher");
+                    expectedDirection = "higher";
                 }
                 else if (guess > randomNumber)
                 {
                     Console.WriteLine("Lower");
+                    expectedDirection = "lower";
                 }
                 else
                 {
@@ -57,38 +78,46 @@ class HighLowGame
                     isGuessedCorrectly = true;
                     winCount++; // Increment win count when the user guesses correctly
                 }
+                lastGuess = guess; // Update lastGuess for the next round
                 attempts++; // Increments attempts by 1
             }
 
-            // Checks if user has guessed correctly or if they have run out of attempts
-            if (attempts >= 5 && !isGuessedCorrectly)
+            // Only ask to play again if the user guessed correctly
+            if (isGuessedCorrectly)
+            {
+                Console.WriteLine($"You have won {winCount} times."); // Display the number of wins
+
+                // Ask to play again
+                while (true)
+                {
+                    Console.WriteLine("Would you like to play again? (yes/no)");
+                    string response = Console.ReadLine().ToLower();
+
+                    if (response == "yes")
+                    {
+                        playAgain = true;
+                        break;
+                    }
+                    else if (response == "no")
+                    {
+                        Console.WriteLine("Goodbye!");
+                        playAgain = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'yes' or 'no'.");
+                    }
+                }
+            }
+            else if (attempts >= AmountofGuesses) // If user has run out of attempts
             {
                 Console.WriteLine("You're a goofball and I don't want to play anymore.");
-                break;
+                playAgain = false; // Set playAgain to false
             }
-
-            Console.WriteLine($"You have won {winCount} times."); // Display the number of wins
-
-            // Loop until a valid response of yes or no is given
-            while (true)
+            else // If kicked out, we already set playAgain to false
             {
-                Console.WriteLine("Would you like to play again? (yes/no)");
-                string response = Console.ReadLine().ToLower();
-
-                if (response == "yes")
-                {
-                    playAgain = true;
-                    break;
-                }
-                else if (response == "no")
-                {
-                    playAgain = false;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Please enter 'yes' or 'no'.");
-                }
+                playAgain = false; // Set playAgain to false
             }
         }
     }
